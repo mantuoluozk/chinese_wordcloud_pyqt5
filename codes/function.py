c69436731486a00089d74f15655eea1b3927624f
mantuoluozk <1741695059@qq.com>
2018-12-18 21:46:23 +0800

    Update function.py

--- codes/function.py
+++ codes/function.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import random
 from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
 import numpy as np
@@ -7,12 +8,13 @@ import matplotlib.pyplot as plt
 import jieba
 from os import path
 
-def run(imgpath,txtpath):
+def run(imgpath,txtpath,color):
+
     # 文本路径名
     text_path = txtpath 
 
     # 中文字体路径名
-    font_path = 'txt/Hiragino.ttf' 
+    font_path = 'ttf/Hiragino.ttf' 
 
     # 背景图片路径名
     img_path = imgpath
@@ -30,13 +32,20 @@ def run(imgpath,txtpath):
     text = ' '.join(jieba.cut(text))
     print(text[:100])
 
-    # 生成对象
-    mask = np.array(Image.open(img_path))
-    wc = WordCloud(mask=mask, font_path=font_path, mode='RGBA', background_color=None).generate(text)
-
-    # 从图片中生成颜色
-    image_colors = ImageColorGenerator(mask)
-    wc.recolor(color_func=image_colors)
+    # 颜色函数
+    def random_color(word, font_size, position, orientation, font_path, random_state):
+        s = 'hsl(0, %d%%, %d%%)' % (random.randint(0, 255), random.randint(0, 255))
+        print(s)
+        return s
+        
+    if color:   #从图片中生成颜色
+        mask = np.array(Image.open(img_path))       #生成对象
+        wc = WordCloud(mask=mask, font_path=font_path, mode='RGBA', background_color=None).generate(text)
+        image_colors = ImageColorGenerator(mask)
+        wc.recolor(color_func=image_colors)
+    else:       #随机颜色
+        mask = np.array(Image.open(img_path))    #生成对象
+        wc = WordCloud(color_func=random_color, mask=mask, font_path=font_path, mode='RGBA', background_color=None).generate(text)
 
     # 显示词云
     plt.imshow(wc, interpolation='bilinear')
